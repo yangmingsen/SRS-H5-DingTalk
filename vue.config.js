dff81f1874a751bc4bd3bdd2acbcb23285336d17
ChenMeng <meng.chen@esrtech.cn>
2020-04-22 16:33:38 +0800

    UPDATE

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8080/api",
         ws: true,
         changeOrigin: true
       }
