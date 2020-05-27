a3f523fa4296b8e47b8ff922f5066e703fcc1fd2
ChenMeng <meng.chen@esrtech.cn>
2020-05-27 15:43:42 +0800

    配置更新

--- vue.config.js
+++ vue.config.js
@@ -2,21 +2,22 @@
  * vue-cli额外自定义配置
  */
 module.exports = {
-    lintOnSave: "warning",
-    devServer: {
-        proxy: {
-            "/api": {
-                target: "http://192.168.163.11:8046",
-                ws: true,
-                changeOrigin: true,
-                pathRewrite: {
-                    "^/api": ""
-                }
-            }
-        },
-        host: '0.0.0.0',
-        disableHostCheck: true,
-        port: 8848
-    }
-
+  lintOnSave: "warning",
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://192.168.163.11:8046",
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    },
+    host: "0.0.0.0",
+    disableHostCheck: true,
+    port: 8848
+  },
+  outputDir: "../dist/dingtalk",
+  publicPath: "/dingtalk/"
 };
